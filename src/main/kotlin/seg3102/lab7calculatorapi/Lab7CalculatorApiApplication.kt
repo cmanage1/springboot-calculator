f6f084952f164b380059a945ea16f37acfbf8b8d
cmanage1 <chethinlm@gmail.com>
2020-11-06 15:16:40 -0500

    lab7- complete

---
+++ src/main/kotlin/seg3102/lab7calculatorapi/Lab7CalculatorApiApplication.kt
@@ -0,0 +1,11 @@
+package seg3102.lab7calculatorapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab7CalculatorApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<Lab7CalculatorApiApplication>(*args)
+}
---
+++ src/test/kotlin/seg3102/lab7calculatorapi/Lab7CalculatorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package seg3102.lab7calculatorapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab7CalculatorApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
